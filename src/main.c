044f13e0904ea6bbfd47443fe30aba4339ecb6fe
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-06-04 15:56:50 +0100

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+const uint LEDPIN = 25;
+
+int main()
+{
+    stdio_init_all();
+
+    gpio_init(LEDPIN);
+    gpio_set_dir(LEDPIN, GPIO_OUT);
+
+    while (1)
+    {
+        gpio_put(LEDPIN, 1);
+        sleep_ms(500);
+        gpio_put(LEDPIN, 0);
+        puts("Hello, World!\n");
+        sleep_ms(500);
+    }
+}
